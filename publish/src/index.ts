2670b8be6340b135e3095ad43d6ab0f19cedd21e
max-vasin <mozg.mayhem@gmail.com>
2021-02-17 23:46:56 +0300

    Test action
    Co-authored-by: Max Vasin <maksim.vasin@devexpress.com>

--- publish/src/index.ts
+++ publish/src/index.ts
@@ -3,6 +3,8 @@ import * as core from '@actions/core'
 export const entry = (): void => {
   core.debug(`message from action!`)
   core.debug(`another message!`)
+  // eslint-disable-next-line no-console
+  console.log(`action output!`)
 }
 
 entry()
