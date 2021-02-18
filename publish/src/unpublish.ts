fa68fa43b25a4260a317ab5fa74b8f5719082790
Max Vasin <maksim.vasin@devexpress.com>
2021-02-18 23:23:19 +0300

    Fix unpublish

--- publish/src/unpublish.ts
+++ publish/src/unpublish.ts
@@ -1,4 +1,3 @@
-import * as core from '@actions/core'
 import { readFileSync } from 'fs'
 import { execSync } from 'child_process'
 import semver from 'semver'
@@ -17,19 +16,15 @@ export const unpublish = async (version: string): Promise<void> => {
     throw Error(`invalid publish version: ${version}`)
   }
 
-  core.debug(`reading package.json`)
   const pkg = JSON.parse(readString('./package.json'))
 
   const { name, private: restricted } = pkg
 
   if (restricted) {
-    core.info(`(${name}) is private package, exiting`)
     return
   }
 
   try {
     exec(`npm unpublish --force ${name}@${publishedVersion.version}`)
-  } catch (error) {
-    core.warning(error)
-  }
+  } catch (error) {}
 }
