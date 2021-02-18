f2bf58b1834559d0b2be6cff58c3e403e421cb4e
Max Vasin <maksim.vasin@devexpress.com>
2021-02-18 20:21:37 +0300

    Post hook

--- deploy/src/post.ts
+++ deploy/src/post.ts
@@ -1,3 +1,15 @@
+import * as core from '@actions/core'
+import { execSync } from 'child_process'
+
 export const post = async (): Promise<void> => {
-  return Promise.resolve()
+  const deploymentId = core.getState('deployment_id')
+
+  try {
+    core.info(`removing cloud application ${deploymentId}`)
+    execSync(`yarn --silent resolve-cloud remove ${deploymentId} --no-wait`, {
+      stdio: 'inherit',
+    })
+  } catch (error) {
+    core.warning(error)
+  }
 }
--- deploy/test/post.test.ts
+++ deploy/test/post.test.ts
@@ -1,5 +1,37 @@
+import { mocked } from 'ts-jest/utils'
+import { execSync } from 'child_process'
+import * as core from '@actions/core'
 import { post } from '../src/post'
 
-test('dummy', async () => {
+jest.mock('child_process')
+jest.mock('@actions/core')
+
+const mExec = mocked(execSync)
+const mCoreGetState = mocked(core.getState)
+
+let jobState: { [key: string]: string }
+
+beforeEach(() => {
+  jobState = {
+    deployment_id: 'deployment-id',
+  }
+
+  mCoreGetState.mockImplementation((name) => jobState[name])
+})
+
+test('invoke cloud application deletion', async () => {
+  await post()
+
+  expect(mExec).toHaveBeenCalled()
+  expect(mExec.mock.calls[0][0]).toMatchInlineSnapshot(
+    `"yarn --silent resolve-cloud remove deployment-id --no-wait"`
+  )
+})
+
+test('tolerate to script execution errors', async () => {
+  mExec.mockImplementationOnce(() => {
+    throw Error('script error')
+  })
+
   await post()
 })
