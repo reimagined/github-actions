cbf2df8d74784d645eb9afaaf38bf1e2b43b8040
Max Vasin <maksim.vasin@devexpress.com>
2021-02-18 19:07:14 +0300

    Enable tests in workflow

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,14 @@ module.exports = {
   rootDir: process.cwd(),
   testMatch: ['**/test/**/*.test.[jt]s?(x)'],
   collectCoverageFrom: ['src/**/*.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
