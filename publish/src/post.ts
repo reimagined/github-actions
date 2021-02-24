115f8f8718a90bf62d45e0d382cc57df7cc394a3
const314 <constantin.panchenko@gmail.com>
2021-02-24 19:35:31 +0300

    implement github unpublishing; replace script calls with direct function calls

--- publish/src/post.ts
+++ publish/src/post.ts
@@ -1,7 +1,5 @@
 import * as core from '@actions/core'
-import { exec } from 'child_process'
 import { unlinkSync, copyFileSync } from 'fs'
-import minimist from 'minimist'
 import { unpublish } from './unpublish'
 import { processWorkspaces } from '../../common/src/utils'
 
@@ -9,13 +7,6 @@ const isTrue = (value: string) =>
   value != null && ['yes', 'true', '1'].includes(value.toLowerCase())
 
 export const post = async (): Promise<void> => {
-  const args = minimist(process.argv.slice(2))
-  const command = args._.length ? args._[0] : ''
-
-  if (command === 'unpublish') {
-    return await unpublish(args.version)
-  }
-
   if (isTrue(core.getInput('unpublish'))) {
     const version = core.getState('version')
     if (!version) {
@@ -25,26 +16,8 @@ export const post = async (): Promise<void> => {
     core.info('removing published packages from the registry')
 
     await processWorkspaces(async (w) => {
-      await new Promise((resolve, reject) => {
-        core.debug(`[${w.name}] executing unpublish script`)
-        exec(
-          `${process.argv[0]} ${process.argv[1]} unpublish --version=${version}`,
-          {
-            cwd: w.location,
-          },
-          (error, stdout, stderr) => {
-            if (error) {
-              return reject(
-                Error(`${error.message}:\nstderr:${stderr}\nstdout:${stdout}`)
-              )
-            }
-            if (stdout) {
-              core.debug(stdout)
-            }
-            resolve(stdout)
-          }
-        )
-      })
+      core.debug(`[${w.name}] executing unpublish`)
+      await unpublish(version, w.location)
     }, core.debug)
   }
 
--- publish/src/unpublish.ts
+++ publish/src/unpublish.ts
@@ -1,6 +1,8 @@
 import { readFileSync } from 'fs'
+import * as core from '@actions/core'
 import { execSync } from 'child_process'
 import semver from 'semver'
+import { getOctokit } from '@actions/github'
 
 const readString = (file: string): string => {
   return readFileSync(file).toString('utf-8')
@@ -10,13 +12,89 @@ const exec = (command: string) => {
   return execSync(command).toString('utf-8')
 }
 
-export const unpublish = async (version: string): Promise<void> => {
+const githubClient = (token: string) => {
+  const github = getOctokit(token)
+
+  const getPackageVersionId = async (packageName: string, version: string) => {
+    const query = `
+    query getVersions($package: String!, $version: String!) {
+      repository(owner:\"reimagined\",name:\"resolve\") {
+        packages(first:1, names: [$packageName]) {
+          nodes {
+            name,
+            id,
+            version(version:$version) {
+              id,
+              version
+            }
+          }
+        }
+      }
+    }`
+    const result: any = await github.graphql(query, {
+      packageName,
+      version,
+      headers: {
+        Accept: 'application/vnd.github.packages-preview+json',
+      },
+    })
+    if (result.repository.packages.nodes.length === 0) {
+      throw Error(`Package is not found in the registry: ${packageName}`)
+    }
+    if (!result.repository.packages.nodes[0].version) {
+      throw Error(`Package version is not found in the registry: ${version}`)
+    }
+    const packageVersionId = result.repository.packages.nodes[0].version.id
+    return packageVersionId
+  }
+
+  const unpublish = async (name: string, version: string) => {
+    const packageVersionId = await getPackageVersionId(name, version)
+
+    const mutation = `
+  mutation deletePackageVersion($packageVersionId: String!) {
+      deletePackageVersion(input: {packageVersionId: $packageVersionId}) {
+          success
+      }
+  }`
+    return await github.graphql(mutation, {
+      packageVersionId,
+      headers: {
+        Accept: 'application/vnd.github.package-deletes-preview+json',
+      },
+    })
+  }
+  return { unpublish }
+}
+
+const unpublishFromGithubRegistry = async (
+  packageName: string,
+  packageVersion: string
+) => {
+  const registryToken = core.getInput('token', { required: true })
+  await githubClient(registryToken).unpublish(packageName, packageVersion)
+}
+
+const unpublishPackage = async (
+  packageName: string,
+  packageVersion: string
+) => {
+  const isGithubRegistry = core.getState('isGithubRegistry')
+  isGithubRegistry
+    ? await unpublishFromGithubRegistry(packageName, packageVersion)
+    : exec(`npm unpublish --force ${packageName}@${packageVersion}`)
+}
+
+export const unpublish = async (
+  version: string,
+  packageLocation = '.'
+): Promise<void> => {
   const publishedVersion = semver.parse(version)
   if (!publishedVersion) {
     throw Error(`invalid publish version: ${version}`)
   }
 
-  const pkg = JSON.parse(readString('./package.json'))
+  const pkg = JSON.parse(readString(`${packageLocation}/package.json`))
 
   const { name, private: restricted } = pkg
 
@@ -25,6 +103,6 @@ export const unpublish = async (version: string): Promise<void> => {
   }
 
   try {
-    exec(`npm unpublish --force ${name}@${publishedVersion.version}`)
+    await unpublishPackage(name, publishedVersion.version)
   } catch (error) {}
 }
