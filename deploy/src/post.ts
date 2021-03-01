30be1c7fdcf0d83e4cb3f2bf16cde24c39272ed2
max-vasin <mozg.mayhem@gmail.com>
2021-03-02 02:13:19 +0300

    Use correct CLI (#34)

--- deploy/src/post.ts
+++ deploy/src/post.ts
@@ -1,17 +1,17 @@
 import * as core from '@actions/core'
-import { execSync } from 'child_process'
+import { getCLI } from '../../common/src/cli'
 
 export const post = async (): Promise<void> => {
   const id = core.getState('app_id')
   const dir = core.getState('app_dir')
-
-  try {
-    core.info(`removing cloud application ${id}`)
-    execSync(`yarn --silent resolve-cloud remove ${id} --no-wait`, {
-      stdio: 'inherit',
-      cwd: dir,
-    })
-  } catch (error) {
-    core.warning(error)
+  if (id != null && dir != null) {
+    try {
+      core.startGroup(`removing cloud deployment: ${id}`)
+      const cli = getCLI(dir, core.getInput('cli_sources'))
+      cli(`rm ${id} --no-wait`, 'inherit')
+      core.endGroup()
+    } catch (error) {
+      core.warning(error)
+    }
   }
 }
