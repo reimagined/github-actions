e4982cf509edc8f84af084e118312c9c7ba7e920
Max Vasin <maksim.vasin@devexpress.com>
2021-03-03 23:17:37 +0300

    Upd

--- release-pr-bot/src/main.ts
+++ release-pr-bot/src/main.ts
@@ -1,5 +1,10 @@
+import * as core from '@actions/core'
 import * as github from '@actions/github'
 
 export const main = (): Promise<void> => {
+  const event = core.getInput('event')
+
+  core.debug(event)
+
   return Promise.resolve()
 }
