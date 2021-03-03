d5e565706bf69cde5e34ca387c1e1fdddf785a64
Max Vasin <maksim.vasin@devexpress.com>
2021-03-03 22:53:58 +0300

    Init

---
+++ release-pr-bot/webpack.config.js
@@ -0,0 +1,14 @@
+const { merge } = require('webpack-merge')
+const configBase = require('../webpack.config-base')
+
+module.exports = merge(configBase, {
+  entry: {
+    main: './src/main-entry.ts',
+    post: './src/post-entry.ts',
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].js',
+  },
+  context: __dirname,
+})
