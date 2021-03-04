87c5a53dbfc9c02869e86001d81c59c2a881325e
Max Vasin <maksim.vasin@devexpress.com>
2021-03-04 16:55:50 +0300

    upd

--- git-release/src/main.ts
+++ git-release/src/main.ts
@@ -1,8 +1,13 @@
 import * as core from '@actions/core'
+import * as path from 'path'
 import { registerPrivateKey } from '../../common/src/ssh'
 
 export const main = async (): Promise<void> => {
-  registerPrivateKey(core.getInput('ssh_private_key', { required: true }))
+  registerPrivateKey(
+    core.getInput('ssh_private_key', { required: true }),
+    path.resolve('bot-github-rsa'),
+    core
+  )
 
   return
 }
