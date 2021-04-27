2f01c2f931aac7bfde671339e0e1173ec70a01c0
Max <mozg.mayhem@gmail.com>
2021-04-27 16:17:26 +0300

    WIP

--- git-release/src/post.ts
+++ git-release/src/post.ts
@@ -2,6 +2,9 @@ import * as core from '@actions/core'
 import * as path from 'path'
 import { parseBoolean } from '../../common/src/utils'
 import { getGit } from '../../common/src/git'
+import { getOctokit } from '@actions/github'
+import { readFileSync } from 'fs'
+import { PushEvent } from '../../common/src/types'
 
 export const post = async (): Promise<void> => {
   const success = parseBoolean(core.getState('success'))
@@ -14,6 +17,10 @@ export const post = async (): Promise<void> => {
       core.info(`successful release, performing tagging and back-merging`)
       const devBranch = core.getInput('development_branch')
       const versionTag = core.getState('version_tag')
+      core.debug(`parsing push event`)
+      const event: PushEvent = JSON.parse(
+        core.getInput(`push_event`, { required: true })
+      )
 
       core.startGroup(`commit release`)
       core.debug(`checking out ${releaseBranch}`)
@@ -45,6 +52,41 @@ export const post = async (): Promise<void> => {
       core.debug(`pushing tag to remote`)
       git(`push --tags`)
       core.endGroup()
+
+      if (parseBoolean(core.getInput('create_github_release'))) {
+        core.startGroup('creating GitHub release')
+        let releaseInfo = ''
+        try {
+          core.debug(`gathering release info`)
+          const changelogSlicer = new RegExp(
+            `(^## \[${versionTag}\][^\n]*$)(.*?)^(## V[0-9]*.[0-9]*.[0-9])`,
+            'gms'
+          )
+          const changelog = readFileSync('./CHANGELOG.md').toString()
+          const slices = changelogSlicer.exec(changelog)
+          if (slices != null) {
+            releaseInfo = slices[2].trim()
+            core.debug(`release info was extracted from CHANGELOG.md`)
+          } else {
+            core.warning(
+              'Changelog slicing failed. Malformed CHANGELOG.md or bad slicing reg exp'
+            )
+          }
+        } catch (e) {
+          core.warning(`Changelog processing failed: ${e.message}`)
+        }
+
+        core.debug(`creating GitHub release`)
+        const octokit = getOctokit(core.getInput('token', { required: true }))
+        await octokit.repos.createRelease({
+          owner: event.repository.owner.name,
+          repo: event.repository.name,
+          tag_name: versionTag,
+          name: versionTag,
+          body: releaseInfo,
+        })
+        core.endGroup()
+      }
 
       core.info(`${versionTag} released successfully`)
     } else {
